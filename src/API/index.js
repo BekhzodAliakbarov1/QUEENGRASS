0bf5f7f5ca11c20fc7a89fabd6e07caf6010730a
Bekhzod <bekobekome@gmail.com>
2021-04-01 09:16:25 +0500

    sdfghjk

--- src/API/index.js
+++ src/API/index.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 export default axios.create({
     // baseURL: 'https://polar-shelf-89571.herokuapp.com/api',
-    baseURL: 'http://bekoome.pythonanywhere.com/api',
+    baseURL: 'https://bekoome.pythonanywhere.com/api',
     headers: {
         'Content-Type': 'application/json'
     }
